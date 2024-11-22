d677e45a82beb7cced345f007e3552382d2f2230
DaddyGanzux <Garm0607@gmail.com>
2024-11-22 08:41:38 -0600

    P023_Gonzalo
    Acr

--- Zap1097_Punteros_v2/Zap1097_Punteros_v2/Zap1097_Punteros_v2.cpp
+++ Zap1097_Punteros_v2/Zap1097_Punteros_v2/Zap1097_Punteros_v2.cpp
@@ -1,56 +1,57 @@
-// Zap1097_Punteros_v2.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-#include<stdio.h>
-#include<iostream>
-#include<string>
+// P008_PUNTEROS_V2.cpp
+// Sintaxys de Memoria Dinamica
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
 #include <locale>
-#include<Windows.h>
+#include <Windows.h>
 //Espacio para declarar funciones
-void imprimir(std::string* titulo, std::string* autor, int* anio_Pub, int tamanio);
-
-int main()
+void imprimir(std::string* titulo, std::string* Autor, int* anio_Pub, int tamanio)
 {
-    setlocale(LC_ALL, "en_MX.EFT-8");
-
-    //Crear punteros para almacenar juegos
-    std::string* titulo = NULL; //Iniciar el puntero en NULL
-    std::string* autor = NULL;
-    int* anio_Pub = NULL;
-    int tamanio = 0;
-    std::cout << "Hello gamer, cuantos juegos quieres guardar?\n";
-    std::cin >> tamanio;
-    //No tenemos que tener memoria cautiva
-    //std::string titulos[100];
-
-
-    //Crear variabl con el tamaño que nesecitamos 
-    titulo = new std::string[tamanio];
-    autor = new std::string[tamanio];
-    anio_Pub = new int[tamanio]; 
-
-    //Metodo para almacenar información
-
     for (int i = 0; i < tamanio; i++)
     {
-        std::cout << "Ingresa el valor del juego número " << i + 1 << std::endl;
-        std::ignore
-        getline(std::cin, titulo[i]);
-        delete[] titulo;
-        delete[] autor;
-        delete[] anio_Pub;
-        titulo = NULL;
-        autor = NULL;
-        anio_Pub = NULL;
+        std::cout << "El titulo del juego numero " << i + 1 << " es: " << titulo[i] << std::endl;
+        std::cout << "El Autor del juego numero " << i + 1 << " es: " << Autor[i] << std::endl;
+        std::cout << "El año de publicación del juego numero " << i + 1 << " es: " << anio_Pub[i] << std::endl;
     }
-
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
+int main()
+{
+    setlocale(LC_ALL, "en_MX.UTF-8");
+    //Crear punteros para almacenar juegos
+    std::string* titulo = NULL; //Inicializar el puntero en NULL
+    std::string* Autor = NULL;
+    int* anio_Publicacion = NULL;
+    int tamanio = 0;
+    std::cout << "Hola gamer cuantos juegos quieres guardar??\n";
+    std::cin >> tamanio;
+    //No tenemos que tener cautiva la memoria
+    //std::string titulos[100];
+    //Crear variables con el tamaño que deseamos
+    titulo = new std::string[tamanio];
+    Autor = new std::string[tamanio];
+    anio_Publicacion = new int[tamanio];
+    //metodo para almacenar información
+    for (int i = 0; i < tamanio; i++)
+    {
+        std::cout << "Ingresa el titulo del Juego número " << i + 1 << std::endl;
+        std::cin.ignore();
+        getline(std::cin, titulo[i]);
+        std::cout << "Ingresa el autor del Juego; " << std::endl;
+        getline(std::cin, Autor[i]);
+        std::cout << "Ingresa el año de publicación del Juego; " << std::endl;
+        std::cin >> anio_Publicacion[i];
+        system("CLS");
+    }
+    //Teoría/Practica de Funciones.
+    //Una función es una nave espacial
+    //Imprimir los datos recolectados
+    imprimir(titulo, Autor, anio_Publicacion, tamanio);
+    delete[] titulo;
+    delete[] Autor;
+    delete[] anio_Publicacion;
+    titulo = NULL;
+    Autor = NULL;
+    anio_Publicacion = NULL;
+}
